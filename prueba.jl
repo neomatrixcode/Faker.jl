3c6dc81a5d1aa9bee4944cc961f837ee09d0109e
codeneomatrix <josuecevedo@gmail.com>
2015-07-16 18:41:30 -0600

    direccion.jl agregando datos a direccion.jl

--- prueba.jl
+++ prueba.jl
@@ -16,7 +16,7 @@ include("color.jl")
 for i = 1:4
 	t= time("%H-%M:%S")
 	d = date("%Y-%m:%d")
-	println(" \n $(prefixe()) $(first_name()) $(last_name()) vive en $(state()) trabaja en $(trabajo()) $(zip_code("OAX")) $(timezone()) $(am_pm())  $(day_of_week()) $(months()) $(month_name()) $(time()) $(t) $(date()) $(d) $(iso8601()) $(date_time()) $(unix_time()) \n numero telefonico $(phone_number()) \ncolores: $(color_name())  $(safe_color_name())  $(hex_color())  $(safe_hex_color())  $(rgb_color_list())  $(rgb_css_color())   \n\n")	
+	println(" \n $(prefixe()) $(first_name()) $(last_name()) vive en $(state()) trabaja en $(trabajo()) $(zip_code("OAX")) $(timezone()) $(am_pm())  $(day_of_week()) $(months()) $(month_name()) $(time()) $(t) $(date()) $(d) $(iso8601()) $(date_time()) $(unix_time()) \n numero telefonico $(phone_number()) \ncolores: $(color_name())  $(safe_color_name())  $(hex_color())  $(safe_hex_color())  $(rgb_color_list())  $(rgb_css_color())   \n\n$(street_name() )")	
 end
 
 
