8fb3167baf3885845a94ae9eddefe6bb0bd017d9
neomatrixcode <josuecevedo@gmail.com>
2021-02-26 20:46:59 -0600

    color refactor

--- src/color.jl
+++ src/color.jl
@@ -3,6 +3,6 @@
     safe_color_name()= executor(data["faker"]["color"]["name"])
     hex_color()= executor(data["faker"]["color"]["code"])
 
-    rgb_color_list()="($(rand(1:255)),$(rand(1:255)),$(rand(1:255)))"
-    rgb_color()="$(rand(1:255)),$(rand(1:255)),$(rand(1:255))"
-    rgb_css_color()="rgb($(rand(1:255)),$(rand(1:255)),$(rand(1:255)))"
+    rgb_color()=string(rand(1:255),",",rand(1:255),",",rand(1:255))
+    rgb_color_list()=string("(",rgb_color(),")")
+    rgb_css_color()=string("rgb(",rgb_color(),")")
