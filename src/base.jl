c245db0be7b85d05932d82b43c2545bdf13d74a7
neomatrixcode <josuecevedo@gmail.com>
2021-02-26 21:30:42 -0600

    base refactor

--- src/base.jl
+++ src/base.jl
@@ -1,35 +1,16 @@
-
-function seed(seed)
-    Random.seed!(seed)
-end
-
+seed(seed) = Random.seed!(seed)
 random_int(;min=0::Int, max=9999::Int)="$(rand(min : max))"
-random_digit()= "$(rand(0:9))"
-random_digit_not_null()= "$(rand(1:9))"
-
-function random_digit_or_empty()
-    if (rand(0:1) == 1)
-        return "$(rand(0:9))"
-    else
-        return " "
-    end
-end
-
-function random_digit_not_null_or_empty()
- if (rand(0:1)==1)
-    return "$(rand(1:9))"
-else
-    return " "
-end
-end
-
-random_number(;digits= 1::Int ) = "$(rand(0 : (10^digits) - 1))"
-random_letter()= (rand(0:1) >= 0.5) ? "$(rand('a':'z'))" : "$(rand('A':'Z'))"
-random_element(elements=("a", "b", "b"))= elements[rand(1: length(elements))]
+random_digit()= string(rand(0:9))
+random_digit_not_null()= string(rand(1:9))
+random_digit_or_empty() = string(rand(append!([' '],collect('0':'9'))))
+random_digit_not_null_or_empty() = string(rand(append!([' '],collect('1':'9'))))
+random_number(;digits= 1::Int ) = string(rand(0 : ((10^digits) - 1)))
+random_letter()= string(rand(append!(collect('a':'z'),collect('A':'Z'))))
+random_element(elements=("a", "b", "b"))= rand(elements)
 
 function randomize_nb_elements(;number=10::Int, le=false::Bool, ge=false::Bool)
     if le==true && ge==true
-        return "$(number)"
+        return string(number)
     end
     if ge==true
         min = 100
@@ -41,12 +22,12 @@ function randomize_nb_elements(;number=10::Int, le=false::Bool, ge=false::Bool)
     else
         max=140
     end
-    "$(((rand(min : max))number / 100) + 1 )"
+    string(((rand(min : max))number / 100) + 1 )
 end
 
 function numerify( text = "####"::String)
     for i in text
-        text= replace(text,"#"=>"$(rand(0:9))",count=1)
+        text= replace(text,"#"=>string(rand(0:9)),count=1)
     end
     return text
 end
