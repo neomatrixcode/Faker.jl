0890024b35a86c396f8c55e3a320abd70ccf7fd7
neomatrixcode <josuecevedo@gmail.com>
2021-04-22 16:21:50 -0500

    refactor file

--- src/file.jl
+++ src/file.jl
@@ -1,23 +1,19 @@
 
-    function mime_type()
+    function mime_type()::String
         executor(data["faker"]["file"]["mime_type"])
     end
+    #param category: application|audio|image|message|model|multipart|text|video
 
-        #param category: application|audio|image|message|model|multipart|text|video
-
-    function file_extension()
+    function file_extension()::String
         executor(data["faker"]["file"]["extension"])
     end
 
-  function file_name( extencion="None")
-        #:param extension: file extension
+    function file_name(extencion::String="None")::String
 
-    if extencion == "None"
-     extencion = file_extension()
-    end
+        if extencion == "None"
+            extencion = file_extension()
+        end
 
-    filename = lexify("????")
-
-    "$(filename).$(extencion)"
+        return string(lexify("????"),".",extencion)
 
     end
