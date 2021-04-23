f6da32e6d38d584834d077c194f1d1aeb35ac355
neomatrixcode <josuecevedo@gmail.com>
2021-04-22 20:48:28 -0500

    refactor phone_number.jl

--- src/phone_number.jl
+++ src/phone_number.jl
@@ -1,8 +1,8 @@
 
-lada_dos() = numerify(executor(data["faker"]["phone_number"]["lada_dos"]))
-lada_tres() = numerify(executor(data["faker"]["phone_number"]["lada_tres"]))
-area_code() = numerify(executor(data["faker"]["phone_number"]["area_code"]))
-exchange_code() = numerify(executor(data["faker"]["phone_number"]["exchange_code"]))
+lada_dos()::String = numerify(executor(data["faker"]["phone_number"]["lada_dos"]))
+lada_tres()::String = numerify(executor(data["faker"]["phone_number"]["lada_tres"]))
+area_code()::String = numerify(executor(data["faker"]["phone_number"]["area_code"]))
+exchange_code()::String = numerify(executor(data["faker"]["phone_number"]["exchange_code"]))
 
-phone_number() = numerify(executor(data["faker"]["phone_number"]["formats"]))
-cell_phone() = numerify(executor(data["faker"]["cell_phone"]["formats"]))
+phone_number()::String = numerify(executor(data["faker"]["phone_number"]["formats"]))
+cell_phone()::String = numerify(executor(data["faker"]["cell_phone"]["formats"]))
